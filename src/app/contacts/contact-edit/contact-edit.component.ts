65c475a73d6c2c87c8b885cd85b48c015ea0ed31
Yoli-Buendia <bue17003@byui.edu>
2024-03-16 22:58:57 -0600

    Download the DocumentEditComponent HTML and CSS files

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-edit',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './contact-edit.component.css'
 })
 export class ContactEditComponent {
-
+  groupContacts: Contact[] = [];
+  onCancel() {
+  }
 }
