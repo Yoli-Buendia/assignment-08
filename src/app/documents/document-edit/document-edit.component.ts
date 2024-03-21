81e922c639932141834b0a65aa938d5833b5c13a
Yoli-Buendia <bue17003@byui.edu>
2024-03-20 20:40:36 -0600

    Implementing the Behavior of the Document Edit Form

--- src/app/documents/document-edit/document-edit.component.ts
+++ src/app/documents/document-edit/document-edit.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { Document } from '../document.model';
-import { NgForm } from '@angular/forms';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { DocumentsService } from '../documents.service';
+
 
 @Component({
   selector: 'app-document-edit',
@@ -11,10 +14,52 @@ export class DocumentEditComponent {
   originalDocument: Document;
   document: Document;
   editMode: boolean = false; 
+  id: string;
 
-  onCancel() {
+
+  constructor(
+    private documentService: DocumentsService,
+    private route: ActivatedRoute,
+    private router: Router,){
   }
-  onSubmit(form: NgForm) {
 
+  ngOnInit(): void {
+    new FormGroup({
+      'title': new FormControl(null, Validators.required),
+      'url': new FormControl(null, Validators.required)
+    })
+
+    this.route.params
+      .subscribe (
+      (params: Params) => {
+        this.id = params['id'];
+        if (!this.id){
+          this.editMode = false;
+          return;
+        }
+        this.originalDocument = this.documentService.getDocument(this.id);
+    
+        if (!this.originalDocument) {
+          return;
+        }
+         this.editMode = true;
+         // clone original document
+         this.document = JSON.parse(JSON.stringify(this.originalDocument)); 
+    }) 
+  }
+
+  onSubmit(form: NgForm) {
+    const value = form.value;
+    this.document = new Document(value.id, value.name, value.description, value.url);
+    if (this.editMode) {
+      this.documentService.updateDocument(this.originalDocument, this.document);
+    } else {
+      this.documentService.addDocument(this.document);
+    }
+    this.router.navigate(['../'], {relativeTo: this.route});
+  }
+  
+  onCancel() {
+    this.router.navigate(['../'], {relativeTo: this.route});
   }
 }
