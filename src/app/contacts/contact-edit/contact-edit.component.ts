dd279ac0de4f1596fb5e0016e88f1d594525565d
Yoli-Buendia <bue17003@byui.edu>
2024-03-20 22:00:42 -0600

    Implementing the Behavior of the Contact Edit Form

--- src/app/contacts/contact-edit/contact-edit.component.ts
+++ src/app/contacts/contact-edit/contact-edit.component.ts
@@ -1,18 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Contact } from '../contact.model';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { ContactsService } from '../contacts.service';
 
 @Component({
   selector: 'app-contact-edit',
   templateUrl: './contact-edit.component.html',
   styleUrl: './contact-edit.component.css'
 })
-export class ContactEditComponent {
- 
+export class ContactEditComponent implements OnInit{
+  originalContact: Contact;
+  contact: Contact;
   groupContacts: Contact[] = [];
-  contact: any;
+  editMode: boolean = false;
+  id: string;
+  
+  constructor(
+    private contactService: ContactsService,
+    private router: Router,
+    private route: ActivatedRoute) {
+    }
+
+  ngOnInit(): void {
+    this.route.params
+      .subscribe((params: Params) => {
+        const id = params["id"];
+        if (id === undefined || id === null) {
+          this.editMode = false;
+          return;
+        }
+
+        this.originalContact = this.contactService.getContact(id);
+        if (this.originalContact === undefined || this.originalContact === null) {
+          return;
+        }
+        this.editMode = true;
+
+        this.contact = JSON.parse(JSON.stringify(this.originalContact));
+
+        if (this.originalContact.group != null) {
+          this.groupContacts = JSON.parse(JSON.stringify(this.originalContact.group));
+        }
+      });
+  }
+ 
   onCancel() {
+    this.router.navigate(["/contacts"]);
   }
   onSubmit(form: NgForm) {
 
